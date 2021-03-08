5b96d8f47ce1f1d3a0e15d868f0e110b8e81320d
Alex Tapia <alejandrovztapia@gmail.com>
2021-03-07 21:19:11 -0600

    Initial Commit

---
+++ AlbumDetail/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlbumDetail
+//
+//  Created by Alex Tapia on 07/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
