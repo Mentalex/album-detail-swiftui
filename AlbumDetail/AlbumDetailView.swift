e90bcb10ece15385f4a26633ffd584c5b1dc63b3
Alex Tapia <alejandrovztapia@gmail.com>
2021-03-10 00:13:53 -0600

    Updated AlbumDetailView to apply grouped subviews and added NavigationView.

--- AlbumDetail/AlbumDetailView.swift
+++ AlbumDetail/AlbumDetailView.swift
@@ -10,54 +10,22 @@ import SwiftUI
 struct AlbumDetailView: View {
   
   var body: some View {
-    ZStack {
-      BackgroundView()
-      
-      VStack {
+    NavigationView {
+      ZStack {
+        BackgroundView()
+          .edgesIgnoringSafeArea(.all)
         
-        Text("Random Access Memories")
-          .font(.system(size: 24, weight: .bold))
-          .foregroundColor(.white)
-          .bold()
-        
-        HStack(alignment: .top)  {
-          
-          Image("album")
-            .resizable()
-            .frame(width: 125, height: 125, alignment: .center)
-          
-          VStack(alignment: .leading) {
-            
-            Text("de Daft Punk")
-              .font(.system(size: 14, weight: .regular))
-              .foregroundColor(.white)
-            
-            Text("Álbum · 2013 · 13 canciones")
-              .font(.system(size: 14, weight: .regular))
-              .foregroundColor(.gray)
-              .padding(.top, 2)
-            
-            HStack {
-              CircleButton(imageName: "heart")
-              Spacer()
-              CircleButton(imageName: "arrow.down")
-              Spacer()
-              CircleButton(imageName: "square.and.arrow.up")
-            }.padding(.top, 12)
-          }
-          .padding(.leading, 4)
-          
-          Spacer()
-        }.padding([.leading, .trailing], 12)
+        ScrollView {
+          AlbumHeaderView()
+        }
       }
-    }.edgesIgnoringSafeArea(.all)
+    }
   }
 }
 
-let gradient = Gradient(colors: [])
-
-struct ContentView_Previews: PreviewProvider {
+struct AlbumDetailView_Previews: PreviewProvider {
   static var previews: some View {
     AlbumDetailView()
   }
 }
+
