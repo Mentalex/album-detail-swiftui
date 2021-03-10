04804dc7ac91709cb241d56c57f0f587535571e3
Alex Tapia <alejandrovztapia@gmail.com>
2021-03-10 00:02:02 -0600

    Added CircleButtonContainer subview.

---
+++ AlbumDetail/Subviews/CicleButtonsContainer.swift
@@ -0,0 +1,27 @@
+//
+//  CicleButtonsContainer.swift
+//  AlbumDetail
+//
+//  Created by Alex Tapia on 10/03/21.
+//
+
+import SwiftUI
+
+struct CicleButtonsContainer: View {
+  
+  var body: some View {
+    HStack {
+      CircleButton(imageName: "heart")
+      Spacer()
+      CircleButton(imageName: "arrow.down")
+      Spacer()
+      CircleButton(imageName: "square.and.arrow.up")
+    }.padding(.top, 12)
+  }
+}
+
+struct CicleButtonsContainer_Previews: PreviewProvider {
+    static var previews: some View {
+        CicleButtonsContainer()
+    }
+}
