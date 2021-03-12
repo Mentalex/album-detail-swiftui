f4b19ad7c630afc6f35bb29b9b1550b2efca5022
Alex Tapia <alejandrovztapia@gmail.com>
2021-03-11 22:08:17 -0600

    Added RoundedButton view.

---
+++ AlbumDetail/Subviews/RoundedButton.swift
@@ -0,0 +1,40 @@
+//
+//  RoundedButton.swift
+//  AlbumDetail
+//
+//  Created by Alex Tapia on 11/03/21.
+//
+
+import SwiftUI
+
+struct RoundedButton: View {
+  
+  private let title: String
+  private let iconName: String
+  private let color: Color
+  
+  init(title: String, iconName: String, color: Color) {
+    self.title = title
+    self.iconName = iconName
+    self.color = color
+  }
+  
+  var body: some View {
+    Button(action: {}, label: {
+      HStack {
+        Image(systemName: iconName)
+        Text(title).fontWeight(.semibold)
+      }
+    })
+    .foregroundColor(.white)
+    .padding()
+    .background(color)
+    .cornerRadius(20)
+  }
+}
+
+struct RoundedButton_Previews: PreviewProvider {
+  static var previews: some View {
+    RoundedButton(title: "REPRODUCIR", iconName: "play.fill", color: Color.red)
+  }
+}
