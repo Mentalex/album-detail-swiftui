3eaeadb5b1c059edb0bfe79be7717e2a24b19cf3
Alex Tapia <alejandrovztapia@gmail.com>
2021-03-21 17:47:01 -0600

    Get scrollable offsetY and updated "Header View" offset to apply Sticky behavior.

--- AlbumDetail/AlbumDetailView.swift
+++ AlbumDetail/AlbumDetailView.swift
@@ -9,17 +9,37 @@ import SwiftUI
 
 struct AlbumDetailView: View {
   
+  /* Constants */
+  private let albumDetailScrollableSpace = "AlbumDetailScrollableSpace"
+  
+  /* States */
+  @State private var offsetY: CGFloat = 0.0
+  
   var body: some View {
     NavigationView {
       VStack {
         AlbumHeaderView()
+          .offset(y: offsetY < 0 ? 0 : offsetY)
         PlayAndShuffleButtonsContainer()
+          .offset(y: offsetY < 0 ? 0 : offsetY)
         
-        ScrollView {
+        ScrollView(showsIndicators: false) {
           ForEach(Data.tracks) { track in
             TrackCellView(track: track)
           }
+          .overlay(
+            // Get offsetY using GeometryReader
+            GeometryReader { proxy -> Color in
+              let minY = proxy.frame(in: .named(albumDetailScrollableSpace)).minY
+              print("Offset Y: \(minY)")
+              DispatchQueue.main.async { offsetY = minY }
+              
+              // Return a view is required, just return a clear color view
+              return Color.clear
+            }
+          )
         }
+        .coordinateSpace(name: albumDetailScrollableSpace)
       }
       .background(BackgroundView().edgesIgnoringSafeArea(.bottom))
       .navigationBarTitle("", displayMode: .inline)
