343ed1488d336681da6eb1d4681cb8e78dda712a
Alex Tapia <alejandrovztapia@gmail.com>
2021-03-21 19:28:49 -0600

    Applied logic to steacky header when scroll until Play and Shuffle buttons container, at moment with hardcoded values, this should be improved.

--- AlbumDetail/AlbumDetailView.swift
+++ AlbumDetail/AlbumDetailView.swift
@@ -12,16 +12,29 @@ struct AlbumDetailView: View {
   /* Constants */
   private let albumDetailScrollableSpace = "AlbumDetailScrollableSpace"
   
-  /* States */
+  /* Variables */
   @State private var offsetY: CGFloat = 0.0
+  @State private var headerHeight: CGFloat = 0.0
   
   var body: some View {
     NavigationView {
-      VStack {
-        AlbumHeaderView()
-          .offset(y: offsetY < 0 ? 0 : offsetY)
-        PlayAndShuffleButtonsContainer()
-          .offset(y: offsetY < 0 ? 0 : offsetY)
+      ZStack(alignment: .top) {
+        VStack {
+          AlbumHeaderView()
+          PlayAndShuffleButtonsContainer()
+        }
+        .zIndex(1)
+        .background(Color.x2A2A2A)
+        // TODO: Calculate correctly this hardcoded value (-188)
+        .offset(y: offsetY < -188 ? -188: offsetY)
+        .overlay(
+          // Get Header Height
+          GeometryReader { proxy -> Color in
+            let height = proxy.frame(in: .named(albumDetailScrollableSpace)).size.height
+            headerHeight = height
+            return Color.clear
+          }
+        )
         
         ScrollView(showsIndicators: false) {
           ForEach(Data.tracks) { track in
@@ -31,22 +44,46 @@ struct AlbumDetailView: View {
             // Get offsetY using GeometryReader
             GeometryReader { proxy -> Color in
               let minY = proxy.frame(in: .named(albumDetailScrollableSpace)).minY
-              print("Offset Y: \(minY)")
-              DispatchQueue.main.async { offsetY = minY }
+//              print("Offset Y: \(minY)")
+              offsetY = minY
               
               // Return a view is required, just return a clear color view
               return Color.clear
             }
           )
         }
+        .background(BackgroundView().edgesIgnoringSafeArea(.bottom))
         .coordinateSpace(name: albumDetailScrollableSpace)
+        .padding(.top, topPaddingScroll())
       }
-      .background(BackgroundView().edgesIgnoringSafeArea(.bottom))
+      .background(Color.x2A2A2A)
       .navigationBarTitle("", displayMode: .inline)
       .navigationBarColor(.x2A2A2A)
     }
   }
+  
+  func topPaddingScroll() -> CGFloat {
+    let padding = headerHeight + offsetY
+    print("Header Height: \(headerHeight)")
+    print("OffsetY: \(offsetY)")
+    print("Top Padding: \(padding)\n")
+    
+    // Limit Max Top Padding
+    if padding > headerHeight {
+      return headerHeight
+    }
+    
+    // Limit Min Top Padding
+    // TODO: Calculate correctly this hardcoded value (88)
+    if padding < 88 {
+      return 88
+    }
+    
+    return padding
+  }
 }
+
+
 
 struct AlbumDetailView_Previews: PreviewProvider {
   static var previews: some View {
